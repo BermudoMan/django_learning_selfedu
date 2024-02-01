ee92045035215c9648ea86c9574d663a3e631fac
BermudoMan <ivan.kurochkin.95@bk.ru>
2024-02-01 18:32:45 +0300

    23/73

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1']
 
 INSTALLED_APPS = [
     'women.apps.WomenConfig',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
